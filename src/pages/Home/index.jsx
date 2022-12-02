ec84cbcc883452e0649fb53a722d441d03bd1beb
sheilagj19 <111460663+SheilaJacquemin@users.noreply.github.com>
2022-12-02 10:04:31 -0300

    first commit

---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import {
+  Card,
+  CardBody,
+  CardHeader,
+  Typography,
+  Button,
+  IconButton,
+  Input,
+  Textarea,
+} from "@material-tailwind/react";
+import { UsersIcon } from "@heroicons/react/24/solid";
+import { PageTitle, Footer } from "@/widgets/layout";
+import { FeatureCard, TeamCard } from "@/widgets/cards";
+import { featuresData, teamData, contactData } from "@/data";
+
+const Home = () => {
+  return (
+    <div>Home</div>
+  )
+}
+
+export default Home 
